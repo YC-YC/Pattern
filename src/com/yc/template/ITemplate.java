65a438b3c600261681ced5b4e25ccb49a479f7ab
YC-YC <yellowc.yc@aliyun.com>
2016-03-28 22:43:31 +0800

    添加注释

--- src/com/yc/template/ITemplate.java
+++ src/com/yc/template/ITemplate.java
@@ -2,6 +2,10 @@ package com.yc.template;
 /**
  *@Author Administrator
  *@Time 2016-3-28 下午10:19:43
+ *
+ *适合有统一的方法流程，有个别方法是共有的，不同的方法定义为protected abstract，让子类去实现
+ *提供惟一的方法供外部调用
+ *添加钩子函数，自定义某些功能
  */
 public abstract class ITemplate {
 
